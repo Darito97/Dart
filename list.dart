b629780a9aed9c98e50169094b4c398b5be25638
David Alonso <davidalonso991@gmail.com>
2024-07-24 22:05:27 -0600

    Add(list): Agregado de listas

---
+++ list.dart
@@ -0,0 +1,10 @@
+void main() {
+  List<String> nombres = ["David", "Karla", "Laura"];
+  print("Los nombres son: $nombres");
+  print("El primer nombre de la lista es: ${nombres[0]}");
+  print("El segundo nombre de la lista es: ${nombres[1]}");
+  print("El tercer nombre de la lista es: ${nombres[2]}");
+
+  int cantidadDeNombres = nombres.length;
+  print("La cantidad de nombres en la lista es: $cantidadDeNombres");
+}
