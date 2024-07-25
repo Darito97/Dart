8b8fd475628cf8fe844aac1d12f766760769b585
David Alonso <davidalonso991@gmail.com>
2024-07-24 21:38:22 -0600

    Add(numbers): Agregado de uso de diferentes tipos de numeros

---
+++ numbers.dart
@@ -0,0 +1,11 @@
+void main() {
+  int num1 = 10;
+  double num2 = 12.4;
+  num num3 = 12;
+  num num4 = 12.5;
+
+  print("Num 1 is: $num1");
+  print("Num 2 is $num2");
+  print("Num 3 is $num3");
+  print("Num 4 is $num4");
+}
