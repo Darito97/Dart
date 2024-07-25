66ee63e6be5e39768fc6be1e814f660e845876d1
David Alonso <davidalonso991@gmail.com>
2024-07-24 21:58:22 -0600

    Add(convertDoubleToInt): Conversion de double en entero

---
+++ convertDoubleToInt.dart
@@ -0,0 +1,7 @@
+void main() {
+  double numero = 10.89;
+  print("El número en double es: $numero y su tipo es ${numero.runtimeType}");
+  int numeroEnInt = numero.toInt();
+  print(
+      "El número en int es: $numeroEnInt y su tipo es ${numeroEnInt.runtimeType}");
+}
