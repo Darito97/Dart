23fdfa459efecc58ea9ef7e056210242ec5edabb
David Alonso <davidalonso991@gmail.com>
2024-07-24 21:35:39 -0600

    Add(typesvariables): Uso de tipos de variables

---
+++ typesvariables.dart
@@ -0,0 +1,15 @@
+void main() {
+  String name = "David";
+  String address = "México";
+  int age = 90;
+  num height = 2.18;
+  bool isMarried = false;
+  const pi = 3.14;
+
+  print("Name is: $name");
+  print("Address is: $address");
+  print("Age is: $age");
+  print("Height is: $height");
+  print("Is married? $isMarried");
+  print("A constant value: $pi");
+}
