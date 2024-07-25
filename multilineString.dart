97431bc79a3b9e52a3994a31fe0ee075a69972d9
David Alonso <davidalonso991@gmail.com>
2024-07-24 21:43:23 -0600

    Add(multilineString): Agregado y uso de string multilinea

---
+++ multilineString.dart
@@ -0,0 +1,9 @@
+void main() {
+  var multilineString = """
+  Esta es una string en multilinea
+  que puede tener varios saltos
+  sin problemas
+""";
+
+  print("String multilinea: $multilineString");
+}
