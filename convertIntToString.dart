dae7b6908e0cfbccb7aa985c1ad39090140372cf
David Alonso <davidalonso991@gmail.com>
2024-07-24 21:55:33 -0600

    Add(convertIntToString): Conversión de un entero a string

---
+++ convertIntToString.dart
@@ -0,0 +1,8 @@
+void main() {
+  int numero = 10;
+  print("El número en int es: $numero");
+
+  String numeroEnString = numero.toString();
+  print("El número en String es: $numeroEnString");
+  print("El tipo de numeroEnString es ${numeroEnString.runtimeType}");
+}
