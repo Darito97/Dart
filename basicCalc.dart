5668b00e5e0c075d9d235b8712970b14b4867e25
David Alonso <davidalonso991@gmail.com>
2024-07-24 21:21:13 -0600

    Add(basicCalc): Operaciones básicas con dart

---
+++ basicCalc.dart
@@ -0,0 +1,14 @@
+void main() {
+  var num1 = 10;
+  var num2 = 15;
+
+  var sum = num1 + num2;
+  var diff = num1 - num2;
+  var prod = num1 * num2;
+  double div = num1 / num2;
+
+  print("The sum is: $sum");
+  print("The diff is: $diff");
+  print("The prod is: $prod");
+  print("The div is: $div");
+}
