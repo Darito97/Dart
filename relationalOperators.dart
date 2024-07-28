ac84461427e9b87abfd0a3e9608066b9e67da8dc
David Alonso <davidalonso991@gmail.com>
2024-07-27 23:55:00 -0600

    Add(relationalOperators): Uso de operadores relacionales

---
+++ relationalOperators.dart
@@ -0,0 +1,20 @@
+void main() {
+  int num1 = 10;
+  int num2 = 5;
+
+  print(num1 == num2);
+
+  ///Igual
+  print(num1 < num2);
+
+  ///Menor que
+  print(num1 > num2);
+
+  ///Mayor que
+  print(num1 <= num2);
+
+  ///Igual o menor que
+  print(num1 >= num2);
+
+  ///Igual o mayor que
+}
