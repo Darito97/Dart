39cf6d2fbde89ba9968e81788ca1ea81b95b6dd7
David Alonso <davidalonso991@gmail.com>
2024-07-28 12:29:53 -0600

    Add(asserts): Agregado y uso de asserts

---
+++ conditions/assert.dart
@@ -0,0 +1,9 @@
+void main() {
+  /// Cuando se esta desarrollando es dificil encontrar los errores por lo cual
+  /// se utilizan los assert para encontrarlos de una manera mas facil
+  int number = 14;
+  assert(number != 14);
+
+  /// Para ejecutar este código en la terminal se necesita agregar
+  /// el flag --enable-asserts
+}
