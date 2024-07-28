5f2899831944e73632eadbdb2f3544ec79cc525d
David Alonso <davidalonso991@gmail.com>
2024-07-27 23:45:17 -0600

    Add(incanddecoperators): Uso de incrementos y decrementos y como funcionan

---
+++ incanddecoperators.dart
@@ -0,0 +1,25 @@
+void main() {
+  int num1 = 0;
+  int num2 = 0;
+
+  num2 = ++num1;
+  print("El valor de num2 es $num2");
+
+  num1 = 0;
+  num2 = 0;
+
+  num2 = num1++;
+  print("El valor de num2 es $num2");
+
+  num1 = 0;
+  num2 = 0;
+
+  num2 = --num1;
+  print("El valor de num2 es $num2");
+
+  num1 = 0;
+  num2 = 0;
+
+  num2 = num1--;
+  print("El valor de num2 es $num2");
+}
