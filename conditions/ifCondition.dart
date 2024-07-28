fd3b8d93e2b88f86de17dfbe02cfaa57dd10c46c
David Alonso <davidalonso991@gmail.com>
2024-07-28 11:42:33 -0600

    Add(conditions): Agregado y uso de condiciones if y if else

---
+++ conditions/ifCondition.dart
@@ -0,0 +1,8 @@
+void main() {
+  int num1 = 15;
+  int num2 = 20;
+
+  if (num1 < num2) {
+    print("$num1 es menor que $num2");
+  }
+}
---
+++ conditions/ifElseCondition.dart
@@ -0,0 +1,10 @@
+void main() {
+  int num1 = 15;
+  int num2 = 20;
+
+  if (num1 > num2) {
+    print("$num1 es mayor que $num2");
+  } else {
+    print("$num1 es menor que $num2");
+  }
+}
