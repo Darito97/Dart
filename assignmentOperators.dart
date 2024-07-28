eb3bef4ffccc85fdaae97d9e4b3f81fd373f6154
David Alonso <davidalonso991@gmail.com>
2024-07-27 23:48:37 -0600

    Add(assignmentOperators): Uso de operadores de asignación

---
+++ assignmentOperators.dart
@@ -0,0 +1,11 @@
+void main() {
+  double age = 27;
+  age += 1;
+  print("Despues de la adición la edad es $age");
+  age -= 1;
+  print("Despues de la resta la edad es $age");
+  age *= 2;
+  print("Despues de la multiplicación por 2 la edad es $age");
+  age /= 2;
+  print("Despues de la división entre 2 la edad es $age");
+}
