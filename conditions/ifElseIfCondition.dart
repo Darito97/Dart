e9d33b3914568c723fde00c4db4214ce211b76a9
David Alonso <davidalonso991@gmail.com>
2024-07-28 11:46:22 -0600

    Add(ifElseIfCondition): Agregado y uso de if else if

---
+++ conditions/ifElseIfCondition.dart
@@ -0,0 +1,12 @@
+void main() {
+  int num1 = 22;
+  int num2 = 17;
+
+  if (num1 > num2) {
+    print("$num1 es mayor que $num2");
+  } else if (num1 < num2) {
+    print("$num2 es mayor que $num1");
+  } else {
+    print("$num1 y $num2 son iguales");
+  }
+}
