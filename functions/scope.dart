dd7048c9e638a1336721c2ca5d977987f5c4b73f
David Alonso <davidalonso991@gmail.com>
2024-08-04 13:02:30 -0600

    Add(scope): Uso de scope y entendimiento del mismo

---
+++ functions/scope.dart
@@ -0,0 +1,8 @@
+String variableGlobal = "Hola, soy una variable global";
+
+void main() {
+  String variableLocal =
+      "Hola, soy una variable que solo funciona dentro de la función que fui declarada";
+  print(variableLocal);
+  print(variableGlobal);
+}
