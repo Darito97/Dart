f80766ef9a4e0851b128683c5a5f82dcfd921cfc
David Alonso <davidalonso991@gmail.com>
2024-08-04 12:57:20 -0600

    Add(anonymousFunction): Uso de funciones anonimas en dart

---
+++ functions/anonymousFunction.dart
@@ -0,0 +1,8 @@
+void main() {
+  const frutas = ["Mango", "Pera", "Fresa", "Durazno"];
+
+  /// Dentro del forEach se esta creando una función anónima
+  frutas.forEach((fruta) {
+    print(fruta);
+  });
+}
