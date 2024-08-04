f38b5d89f9e1d06c23ba85d128524327a69b17de
David Alonso <davidalonso991@gmail.com>
2024-08-04 11:57:00 -0600

    Add(forLoop): Uso de asMap para obtener un mapa de valores con indice

--- loops/forLoop.dart
+++ loops/forLoop.dart
@@ -4,4 +4,9 @@ void main() {
   for (String nombre in nombres) {
     print("Hola $nombre");
   }
+
+  /// el metodo asMap() convierte la lista en un mapa que tiene indice y valor
+  nombres
+      .asMap()
+      .forEach((index, nombre) => print("Hola $nombre, tu índice es $index"));
 }
