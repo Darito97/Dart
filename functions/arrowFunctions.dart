f6ebaee5646cf4d508fdbaa9578deca8f8b93e9f
David Alonso <davidalonso991@gmail.com>
2024-08-04 13:00:00 -0600

    Add(arrowFunctions): Uso de funciones flecha en dart

---
+++ functions/arrowFunctions.dart
@@ -0,0 +1,8 @@
+int sumarTresNumeros(int numero1, int numero2, int numero3) =>
+    numero1 + numero2 + numero3;
+void main() {
+  int num1 = 1;
+  int num2 = 4;
+  int num3 = 8;
+  print(sumarTresNumeros(num1, num2, num3));
+}
