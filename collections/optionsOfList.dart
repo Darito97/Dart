3a1c0cee16f4cd448fa3bc22d00775b378eb5131
David Alonso <davidalonso991@gmail.com>
2024-08-07 19:46:58 -0600

    Add(optionsOfList): Uso de diferentes tipos de lista y algunas operaciones

--- collections/optionsOfList.dart
+++ collections/optionsOfList.dart
@@ -6,4 +6,21 @@ void main() {
   // Ejemplo de obtencion de index de un valor en una lista
   var index = list.indexOf(55);
   print(index);
+
+  // Ejemplo de obtención de la longitud de una lista
+  var lengthOfList = list.length;
+  print(lengthOfList);
+
+  // Cambiando un valor en una lista
+  print(list[2]);
+  list[2] = 100;
+  print("Nuevo valor de list[2]: ${list[2]}");
+
+  // Ejemplo de una lista mutable
+  List<String> nombres = ["David", "Karla", "Laura"];
+  print(nombres);
+
+  const List<String> nombresConstantes = ["David", "Karla", "Laura"];
+  // nombresConstantes[1] = "Felix" // Esto no se puede hacer con estas listas
+  print(nombresConstantes);
 }
