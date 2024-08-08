60ca2b90646694c96e056680805810aa0e9cc358
David Alonso <davidalonso991@gmail.com>
2024-08-07 19:42:33 -0600

    Add(optionsOfList): Agregado de obtenido de index de un valor en una lista

--- collections/optionsOfList.dart
+++ collections/optionsOfList.dart
@@ -2,4 +2,8 @@ void main() {
   // Ejemplo de acceso a un valor de una lista
   var list = [55, 45, 555, 44, 26];
   print(list[2]);
+
+  // Ejemplo de obtencion de index de un valor en una lista
+  var index = list.indexOf(55);
+  print(index);
 }
